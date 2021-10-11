cad98ca3fa364754cd734646e6938dcdf81c650b
Robert Ancell <robert.ancell@gmail.com>
2021-10-11 21:52:22 +1300

    UMutableBytes -> UtMutableUint8List + Immutable

--- src/main.c
+++ src/main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 #include "ut-list.h"
-#include "ut-mutable-bytes.h"
+#include "ut-mutable-uint8-list.h"
 #include "ut-uint8-list.h"
 
 int main(int argc, char **argv) {
-  UtObject *bytes = ut_mutable_bytes_new();
-  ut_mutable_bytes_append(bytes, 0x01);
-  ut_mutable_bytes_append(bytes, 0x02);
-  ut_mutable_bytes_append(bytes, 0x03);
-  printf("%zi\n", ut_list_get_length(bytes));
-  printf("%02X\n", ut_uint8_list_get_data(bytes)[1]);
-  ut_object_unref(bytes);
+  UtObject *list = ut_mutable_uint8_list_new();
+  ut_mutable_uint8_list_append(list, 0x01);
+  ut_mutable_uint8_list_append(list, 0x02);
+  ut_mutable_uint8_list_append(list, 0x03);
+  printf("%zi\n", ut_list_get_length(list));
+  printf("%02X\n", ut_uint8_list_get_data(list)[1]);
+  ut_object_unref(list);
 
   return 0;
 }
---
+++ src/ut-mutable-uint8-list.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+#include "ut-object.h"
+
+#pragma once
+
+UtObject *ut_mutable_uint8_list_new();
+
+void ut_mutable_uint8_list_append(UtObject *object, uint8_t data);
+
+void ut_mutable_uint8_list_append_block(UtObject *object, const uint8_t *data,
+                                        size_t data_length);
+
+void ut_mutable_uint8_list_insert(UtObject *object, size_t index, uint8_t data);
+
+void ut_mutable_uint8_list_insert_block(UtObject *object, size_t index,
+                                        const uint8_t *data,
+                                        size_t data_length);
+
+const uint8_t *ut_mutable_uint8_list_get_data(UtObject *object);
+
+bool ut_object_is_mutable_uint8_list(UtObject *object);
