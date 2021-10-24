9045f53330804e43e2908c73a7e1e5fac892423a
Robert Ancell <robert.ancell@gmail.com>
2021-10-24 14:41:28 +1300

    Fix broken UtString refactoring

--- src/ut-string.c
+++ src/ut-string.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include "ut-constant-utf8-string.h"
+#include "ut-general-error.h"
 #include "ut-object-private.h"
 #include "ut-string.h"
 #include "ut-uint32-array.h"
@@ -42,7 +43,7 @@ char *ut_string_take_text(UtObject *object) {
 UtObject *ut_string_get_code_points(UtObject *object) {
   const char *text = ut_string_get_text(object);
   size_t text_length = strlen(text);
-  UtObject *code_points = ut_uint32_array_new();
+  UtObjectRef code_points = ut_uint32_array_new();
   size_t offset = 0;
   while (offset < text_length) {
     uint8_t byte1 = text[offset];
@@ -51,22 +52,22 @@ UtObject *ut_string_get_code_points(UtObject *object) {
       offset++;
     } else if ((byte1 & 0xe0) == 0xc0) {
       if (text_length - offset < 2) {
-        return code_points;
+        return ut_general_error_new("Invalid UTF-8");
       }
       uint8_t byte2 = text[offset + 1];
       if ((byte2 & 0xc0) != 0x80) {
-        return code_points;
+        return ut_general_error_new("Invalid UTF-8");
       }
       ut_uint32_array_append(code_points, (byte1 & 0x1f) << 6 | (byte2 & 0x3f));
       offset += 2;
     } else if ((byte1 & 0xf0) == 0xe0) {
       if (text_length - offset < 3) {
-        return code_points;
+        return ut_general_error_new("Invalid UTF-8");
       }
       uint8_t byte2 = text[offset + 1];
       uint8_t byte3 = text[offset + 2];
       if ((byte2 & 0xc0) != 0x80 || (byte3 & 0xc0) != 0x80) {
-        return code_points;
+        return ut_general_error_new("Invalid UTF-8");
       }
       ut_uint32_array_append(code_points, (byte1 & 0x0f) << 12 |
                                               (byte2 & 0x3f) << 6 |
@@ -74,25 +75,25 @@ UtObject *ut_string_get_code_points(UtObject *object) {
       offset += 3;
     } else if ((byte1 & 0xf8) == 0xf0) {
       if (text_length - offset < 4) {
-        return code_points;
+        return ut_general_error_new("Invalid UTF-8");
       }
       uint8_t byte2 = text[offset + 1];
       uint8_t byte3 = text[offset + 2];
       uint8_t byte4 = text[offset + 3];
       if ((byte2 & 0xc0) != 0x80 || (byte3 & 0xc0) != 0x80 ||
           (byte4 & 0xc0) != 0x80) {
-        return code_points;
+        return ut_general_error_new("Invalid UTF-8");
       }
       ut_uint32_array_append(code_points,
                              (byte1 & 0x07) << 18 | (byte2 & 0x3f) << 12 |
                                  (byte3 & 0x3f) << 6 | (byte4 & 0x3f));
       offset += 4;
     } else {
-      return code_points;
+      return ut_general_error_new("Invalid UTF-8");
     }
   }
 
-  return code_points;
+  return ut_object_ref(code_points);
 }
 
 UtObject *ut_string_get_utf8(UtObject *object) {
