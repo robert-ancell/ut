9a33f91a6957051b9f08c38732aa6ad9855f87d2
Robert Ancell <robert.ancell@gmail.com>
2021-10-29 22:13:13 +1300

    Fix ut_object_is_local_file

--- src/ut-local-file.c
+++ src/ut-local-file.c
@@ -143,6 +143,6 @@ UtObject *ut_local_file_new(const char *path) {
   return object;
 }
 
-bool ut_object_is_file(UtObject *object) {
+bool ut_object_is_local_file(UtObject *object) {
   return ut_object_is_type(object, &object_interface);
 }
