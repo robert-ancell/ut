d54cdcc01caa9fddbf3fd3ddb49623b3cfb506d6
Robert Ancell <robert.ancell@gmail.com>
2021-11-15 22:46:51 +1300

    Only allow a single reader for input streams

--- src/ut-fd-input-stream.c
+++ src/ut-fd-input-stream.c
@@ -17,7 +17,6 @@ typedef struct {
   UtObject object;
   int fd;
   UtObject *read_buffer;
-  size_t read_buffer_length;
   size_t block_size;
   UtObject *watch_cancel;
   UtInputStreamCallback callback;
@@ -28,40 +27,33 @@ typedef struct {
 static void read_cb(void *user_data) {
   UtFdInputStream *self = user_data;
 
-  if (!ut_cancel_is_active(self->cancel)) {
-    // Make space to read a new block.
-    ut_list_resize(self->read_buffer,
-                   self->read_buffer_length + self->block_size);
-
-    // Read a block.
-    ssize_t n_read = read(self->fd,
-                          ut_uint8_array_get_data(self->read_buffer) +
-                              self->read_buffer_length,
-                          self->block_size);
-    assert(n_read >= 0);
-    self->read_buffer_length += n_read; // FIXME: No longer required, just use
-                                        // ut_list_get_length(self->read_buffer)
-    ut_list_resize(self->read_buffer, self->read_buffer_length);
-
-    // No more data to read.
-    bool complete = false;
-    if (n_read == 0) {
-      ut_cancel_activate(self->watch_cancel);
-      complete = true;
-    }
-
-    while (true) {
-      size_t n_used =
-          self->callback(self->user_data, self->read_buffer, complete);
-      ut_list_remove(self->read_buffer, 0, n_used);
-      self->read_buffer_length -= n_used;
-
-      if (ut_cancel_is_active(self->cancel) || self->read_buffer_length == 0 ||
-          !complete) {
-        break;
-      }
-    }
+  // Stop listening for read events when consumer no longer wants them.
+  if (ut_cancel_is_active(self->cancel)) {
+    ut_cancel_activate(self->watch_cancel);
+    return;
   }
+
+  // Make space to read a new block.
+  size_t buffer_length = ut_list_get_length(self->read_buffer);
+  ut_list_resize(self->read_buffer, buffer_length + self->block_size);
+
+  // Read a block.
+  ssize_t n_read =
+      read(self->fd, ut_uint8_array_get_data(self->read_buffer) + buffer_length,
+           self->block_size);
+  assert(n_read >= 0);
+  buffer_length += n_read;
+  ut_list_resize(self->read_buffer, buffer_length);
+
+  // No more data to read.
+  bool complete = false;
+  if (n_read == 0) {
+    ut_cancel_activate(self->watch_cancel);
+    complete = true;
+  }
+
+  size_t n_used = self->callback(self->user_data, self->read_buffer, complete);
+  ut_list_remove(self->read_buffer, 0, n_used);
 
   // Stop listening for read events when consumer no longer wants them.
   if (ut_cancel_is_active(self->cancel)) {
@@ -69,59 +61,12 @@ static void read_cb(void *user_data) {
   }
 }
 
-static void buffered_read_cb(void *user_data) {
-  UtFdInputStream *self = user_data;
-  if (ut_cancel_is_active(self->cancel)) {
-    return;
-  }
-
-  // If have buffered data, process that first.
-  if (self->read_buffer_length > 0) {
-    size_t n_used = self->callback(self->user_data, self->read_buffer, false);
-    ut_list_remove(self->read_buffer, 0, n_used);
-    self->read_buffer_length -= n_used;
-
-    if (ut_cancel_is_active(self->cancel)) {
-      return;
-    }
-  }
-
-  // Now read more data from the file descriptor.
-  if (self->watch_cancel != NULL) {
-    ut_cancel_activate(self->watch_cancel);
-    ut_object_unref(self->watch_cancel);
-  }
-  self->watch_cancel = ut_cancel_new();
-  ut_event_loop_add_read_watch(self->fd, read_cb, self, self->watch_cancel);
-}
-
-static void start_read(UtFdInputStream *self, UtInputStreamCallback callback,
-                       void *user_data, UtObject *cancel) {
-  // Clean up after the previous read.
-  if (ut_cancel_is_active(self->cancel)) {
-    self->callback = NULL;
-    self->user_data = NULL;
-    ut_object_clear(&self->cancel);
-  }
-
-  assert(self->callback == NULL);
-
-  self->callback = callback;
-  self->user_data = user_data;
-  self->cancel = ut_object_ref(cancel);
-
-  // The read might have been started inside a callback, so wait for that to
-  // complete.
-  ut_event_loop_add_delay(0, buffered_read_cb, self, cancel);
-}
-
 static void ut_fd_input_stream_init(UtObject *object) {
   UtFdInputStream *self = (UtFdInputStream *)object;
   self->fd = -1;
   self->read_buffer = ut_uint8_array_new();
-  self->read_buffer_length = 0;
   self->block_size = 4096;
-  self->watch_cancel = NULL;
+  self->watch_cancel = ut_cancel_new();
   self->callback = NULL;
   self->user_data = NULL;
   self->cancel = NULL;
@@ -144,7 +89,13 @@ static void ut_fd_input_stream_read(UtObject *object,
   assert(self->fd >= 0);
   assert(callback != NULL);
 
-  start_read(self, callback, user_data, cancel);
+  assert(self->callback == NULL);
+
+  self->callback = callback;
+  self->user_data = user_data;
+  self->cancel = ut_object_ref(cancel);
+
+  ut_event_loop_add_read_watch(self->fd, read_cb, self, self->watch_cancel);
 }
 
 static UtInputStreamInterface input_stream_interface = {
