397e2e4df4fafab1b2da233970e8f28cd32ed98a
Robert Ancell <robert.ancell@gmail.com>
2021-11-22 08:59:07 +1300

    Rename variable

--- src/deflate/ut-deflate-decoder.c
+++ src/deflate/ut-deflate-decoder.c
@@ -106,27 +106,27 @@ static bool read_block_header(UtDeflateDecoder *self, UtObject *data,
     self->state = DECODER_STATE_ERROR;
     return true;
   case 2: {
-    UtObjectRef distance_code_lengths = ut_uint8_list_new();
+    UtObjectRef distance_code_widths = ut_uint8_list_new();
     for (size_t symbol = 0; symbol < 30; symbol++) {
-      ut_uint8_list_append(distance_code_lengths, 5);
+      ut_uint8_list_append(distance_code_widths, 5);
     }
-    self->distance_decoder = ut_huffman_decoder_new(distance_code_lengths);
+    self->distance_decoder = ut_huffman_decoder_new(distance_code_widths);
 
-    UtObjectRef literal_or_length_code_lengths = ut_uint8_list_new();
+    UtObjectRef literal_or_length_code_widths = ut_uint8_list_new();
     for (size_t symbol = 0; symbol <= 143; symbol++) {
-      ut_uint8_list_append(literal_or_length_code_lengths, 8);
+      ut_uint8_list_append(literal_or_length_code_widths, 8);
     }
     for (size_t symbol = 144; symbol <= 255; symbol++) {
-      ut_uint8_list_append(literal_or_length_code_lengths, 9);
+      ut_uint8_list_append(literal_or_length_code_widths, 9);
     }
     for (size_t symbol = 256; symbol <= 279; symbol++) {
-      ut_uint8_list_append(literal_or_length_code_lengths, 7);
+      ut_uint8_list_append(literal_or_length_code_widths, 7);
     }
     for (size_t symbol = 280; symbol <= 287; symbol++) {
-      ut_uint8_list_append(literal_or_length_code_lengths, 8);
+      ut_uint8_list_append(literal_or_length_code_widths, 8);
     }
     self->literal_or_length_decoder =
-        ut_huffman_decoder_new(literal_or_length_code_lengths);
+        ut_huffman_decoder_new(literal_or_length_code_widths);
 
     self->state = DECODER_STATE_LITERAL_OR_LENGTH;
     return true;
