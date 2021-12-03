20b6d7f0c75019f96e4ef38aeb2b135f5cb7d1e2
Robert Ancell <robert.ancell@gmail.com>
2021-12-03 22:58:17 +1300

    Make DBus example better

--- src/dbus/ut-dbus-client-test.c
+++ src/dbus/ut-dbus-client-test.c
@@ -14,13 +14,19 @@ static void ping_cb(void *user_data, UtObject *out_args) {
 
   ut_cstring_ref text = ut_object_to_string(out_args);
   printf("%s\n", text);
+
+  ut_event_loop_return(NULL);
 }
 
 int main(int argc, char **argv) {
-  client = ut_dbus_client_new_system();
+  client = ut_dbus_client_new_session();
+  UtObjectRef args = ut_list_new_with_data_take(
+      ut_string_new(""), ut_uint32_new(0), ut_string_new(""),
+      ut_string_new("Hello World!"), ut_string_new(""), ut_dbus_array_new("s"),
+      ut_dbus_dict_new("s", "v"), ut_int32_new(-1), NULL);
   ut_dbus_client_call_method(
-      client, "org.freedesktop.DBus", "/org/freedesktop/DBus",
-      "org.freedesktop.DBus.Peer", "Ping", NULL, ping_cb, NULL, NULL);
+      client, "org.freedesktop.Notifications", "/org/freedesktop/Notifications",
+      "org.freedesktop.Notifications", "Notify", args, ping_cb, NULL, NULL);
 
   ut_event_loop_run();
 
