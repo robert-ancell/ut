2cac2abc222d97753f964aee6c45c7a08b206370
Robert Ancell <robert.ancell@gmail.com>
2021-12-03 22:33:25 +1300

    Allow set_active method to be optional for input streams

--- src/ut-input-stream.c
+++ src/ut-input-stream.c
@@ -100,7 +100,9 @@ void ut_input_stream_set_active(UtObject *object, bool active) {
   UtInputStreamInterface *stream_interface =
       ut_object_get_interface(object, &ut_input_stream_id);
   assert(stream_interface != NULL);
-  stream_interface->set_active(object, active);
+  if (stream_interface->set_active != NULL) {
+    stream_interface->set_active(object, active);
+  }
 }
 
 bool ut_object_implements_input_stream(UtObject *object) {
