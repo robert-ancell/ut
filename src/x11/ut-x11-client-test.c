0f117acb026b017af2953b6322dd922624a8848b
Robert Ancell <robert.ancell@gmail.com>
2022-01-17 22:47:35 +1300

    Make able to create and use pixmaps

--- src/x11/ut-x11-client-test.c
+++ src/x11/ut-x11-client-test.c
@@ -3,11 +3,22 @@
 
 #include "ut.h"
 
+static size_t width = 255;
+static size_t height = 255;
+
 static UtObject *client = NULL;
+static uint32_t window = 0;
+static uint32_t pixmap = 0;
+static uint32_t gc = 0;
 
 static void event_cb(void *user_data, UtObject *event) {
   ut_cstring_ref s = ut_object_to_string(event);
   printf("%s\n", s);
+
+  if (ut_object_is_x11_expose(event)) {
+    ut_x11_client_copy_area(client, pixmap, window, gc, 0, 0, 0, 0, width,
+                            height);
+  }
 }
 
 static void error_cb(void *user_data, UtObject *error) {
@@ -48,8 +59,25 @@ static void connect_cb(void *user_data, UtObject *error) {
 
   ut_x11_client_list_extensions(client, list_extensions_cb, NULL, NULL);
 
-  uint32_t window = ut_x11_client_create_window(client, 0, 0, 640, 480);
+  window = ut_x11_client_create_window(client, 0, 0, width, height);
   ut_x11_client_map_window(client, window);
+
+  pixmap = ut_x11_client_create_pixmap(client, window, width, height, 24);
+  gc = ut_x11_client_create_gc(client, pixmap);
+  size_t pixmap_data_length = width * height * 4;
+  uint8_t pixmap_data[pixmap_data_length];
+  for (size_t y = 0; y < height; y++) {
+    for (size_t x = 0; x < width; x++) {
+      uint8_t *pixel = pixmap_data + (y * width * 4) + (x * 4);
+      pixel[0] = 255 * x / width;
+      pixel[1] = 255 * y / height;
+      pixel[2] = 255;
+      pixel[3] = 255;
+    }
+  }
+  ut_x11_client_put_image(client, pixmap, gc, UT_X11_IMAGE_FORMAT_Z_PIXMAP,
+                          width, height, 24, 0, 0, pixmap_data,
+                          pixmap_data_length);
 }
 
 int main(int argc, char **argv) {
